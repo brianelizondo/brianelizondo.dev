d514745ec8579cd116432b7136e1d804b8bfd1ed
Brian Elizondo <brian_elizondo@msn.com>
2023-05-23 19:36:21 -0400

    set text sizing by screen size, general css rules and Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,22 @@ import './App.css';
 import { Container, Row, Col } from 'react-bootstrap';
 
 import Hero from './components/Hero';
+import CustomNavbar from './components/Navbar';
 
 const App: React.FC = () => {
     return (
-        <Container fluid>
-            <Row className="justify-content-md-center mt-5">
-                <Col xs={12} md={4}>
-                    <Hero />
-                </Col>
-                <Col xs={1} md={1}></Col>
-                <Col xs={12} md={6}>
-                
-                </Col>
-            </Row>
+    <Container fluid className="App">
+        <Row className="justify-content-md-center">
+            <Col xs={12} md={4}>
+                <Hero />
+                <CustomNavbar />
+            </Col>
+            <Col xs={1} md={1}></Col>
+            <Col xs={12} md={6}>
             
-            
-            
-            
-        </Container>
+            </Col>
+        </Row>
+    </Container>
   );
 }
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Hero.css';
+import { Container, Row } from 'react-bootstrap';
 
 const Hero: React.FC = () => {
     return (
-        <div className="Hero position-sticky top-0">
-            
-        </div>
+    <Container className="Hero position-sticky top-0 pt-5">
+        <Row>
+            <h1>Brian Elizondo</h1>
+        </Row>
+        <Row>
+            <h3>Full-Stack Software Engineer</h3>
+        </Row>
+        <Row>
+            <p>A passionate Full-Stack Software Engineer with love for solving any type of problem or idea in a creative and efficient way, always with the goal of obtaining better results than expected.</p>
+        </Row>
+    </Container>
   );
 }
 
