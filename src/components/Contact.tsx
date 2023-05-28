a25b8ee6a5a8645020f14c3b1acf93f574da18a9
Brian Elizondo <brian_elizondo@msn.com>
2023-05-28 19:12:11 -0400

    Ajust contact style and refactor code

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import './Contact.css';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import { ContactIcon } from '../helpers/NavbarIcons';
 
-
 const Contact: React.FC = () => {
+    const handleEmailClick = () => {
+        window.location.href = "mailto:me@brianelizondo.dev";
+    };
+    
     return (
     <Container id="contact" className="Contact">
         <Row className="ContactTitle">
             <h4><ContactIcon /> Get In Contact</h4>
         </Row>
         <Row className="ContactContent">
-            <Col xs={12} md={12}>
-                <div className="ContactText">
-                    <p>Got a question or proposal, or just want to say hello? Go ahead.</p>
-                    <p>I am currently open to new opportunities!</p>
-                </div>
-                <div className="ContactButton"><a href="mailto:me@brianelizondo.dev">Say Hello</a></div>
-            </Col>
+            <div className="ContactText">
+                <p>Got a question or proposal, or just want to say hello? Go ahead.</p>
+                <p>I am currently open to new opportunities!</p>
+            </div>
+            <div className="ContactButton" onClick={handleEmailClick}>Say Hello</div>
         </Row>
     </Container>
   );
 }
 
-export default Contact;
+export default Contact; 
