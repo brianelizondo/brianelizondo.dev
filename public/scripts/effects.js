2a4d944d3ce4d4ad491f076b8189620264439dd7
Brian Elizondo <brian_elizondo@msn.com>
2023-05-29 11:12:10 -0400

    Ajust bg image and effect

--- public/scripts/effects.js
+++ public/scripts/effects.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
    $(document).mousemove(function(event) {
         let posicionX = event.clientX / $(window).width() * 100;
         let posicionY = (event.clientY + 50) / $(window).width() * 100;
-        $('.AppBgEffect').css('background-image', `radial-gradient(circle at ${posicionX}% ${posicionY}%, rgba(0, 0, 0, 0.75) 0%, rgba(0, 0, 0, 0.98) 8%`);
+        $('.AppBgEffect').css('background-image', `radial-gradient(circle at ${posicionX}% ${posicionY}%, rgba(0, 0, 0, 0.75) 0%, rgba(0, 0, 0, 0.99) 15%`);
     });
 });
